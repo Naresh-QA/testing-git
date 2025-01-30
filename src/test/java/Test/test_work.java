833705de6d76dc39fa066bc19cc2efb36d429c07
kodaru110 <134582419+kodaru110@users.noreply.github.com>
2025-01-30 10:15:40 +0530

    Update test_work.java

--- src/test/java/Test/test_work.java
+++ src/test/java/Test/test_work.java
@@ -1,5 +1,12 @@
 package Test;
 
 public class test_work {
-
+	public static void main(String[] args) {
+		int a=0;
+		for(int i=1;i<=10;i++) {
+			a+=i;
+		}
+			System.out.println("value of a"+a);
+			
+}
 }
